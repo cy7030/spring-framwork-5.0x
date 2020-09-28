80226c47822544fb66c4da75e74872c35c9ac663
yong.chen <chenyong@happycfc.com>
2020-09-28 12:23:04 +0800

    init

---
+++ mytest/src/main/java/yong/chen/ioc/bean/MyTestBean.java
@@ -0,0 +1,17 @@
+package yong.chen.ioc.bean;
+
+/**
+ * @author 陈勇【chenyong@happycfc.com】
+ * @Date 2020/9/28 9:51
+ */
+public class MyTestBean {
+	private String testStr = "testStr";
+
+	public String getTestStr() {
+		return testStr;
+	}
+
+	public void setTestStr(String testStr) {
+		this.testStr = testStr;
+	}
+}
---
+++ mytest/src/test/java/yong/chen/ioc/bean/BeanFactoryTest.java
@@ -0,0 +1,22 @@
+package yong.chen.ioc.bean;
+
+import org.junit.Test;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+/**
+ * @author 陈勇【chenyong@happycfc.com】
+ * @Date 2020/9/28 9:51
+ */
+@SuppressWarnings("deprecation")
+public class BeanFactoryTest {
+	@Test
+	public void testSimpleLoad() {
+		Resource resource = new ClassPathResource("beanFactoryTest.xml");
+		BeanFactory beanFactory = new XmlBeanFactory(resource);
+		MyTestBean myTestBean = (MyTestBean) beanFactory.getBean("myTestBean");
+		System.out.println(myTestBean.getTestStr());
+	}
+}
