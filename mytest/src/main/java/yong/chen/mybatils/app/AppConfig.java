2207fa6b99ab3b33e6a85606c52298d5c5f50854
yong.chen <chenyong@happycfc.com>
2020-09-29 13:05:39 +0800

    init

---
+++ mytest/src/main/java/yong/chen/mybatils/app/AppConfig.java
@@ -0,0 +1,15 @@
+//package yong.chen.mybatils.app;
+//
+//import org.springframework.context.annotation.ComponentScan;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.context.annotation.Import;
+//
+///**
+// * @author 陈勇【chenyong@happycfc.com】
+// * @Date 2020/9/28 19:53
+// */
+//@Configuration
+//@ComponentScan("yong.chen")
+////@Import()
+//public class AppConfig {
+//}
---
+++ mytest/src/main/java/yong/chen/mybatils/test/Test.java
@@ -0,0 +1,19 @@
+//package yong.chen.mybatils.test;
+//
+//import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+//import yong.chen.mybatils.IndexDao;
+//import yong.chen.mybatils.app.AppConfig;
+//
+///**
+// * @author 陈勇【chenyong@happycfc.com】
+// * @Date 2020/9/28 19:40
+// */
+//public class Test {
+//	public static void main(String[] args) {
+//		AnnotationConfigApplicationContext annotationConfigApplicationContext = new AnnotationConfigApplicationContext();
+//		annotationConfigApplicationContext.register(AppConfig.class);
+//		annotationConfigApplicationContext.refresh();
+//		annotationConfigApplicationContext.getBean(IndexDao.class).query();
+//		System.out.println(annotationConfigApplicationContext.getBean("indexDao").getClass().getName());
+//	}
+//}
